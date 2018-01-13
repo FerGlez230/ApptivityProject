1497abd296500cdb54159596d51eddbdb5090387
FerGlez230 <ferglez230@gmail.com>
2018-01-12 22:15:16 -0600

    Ubicacion en registro :)

--- js/general.js
+++ js/general.js
@@ -1,6 +1,73 @@
 //@prepros-prepend jquery-3.1.1.js
 
-$(document).ready(function(){
-  alert("Hola");
-  $('.headerIndex').css({ 'width': $(window).width() + "px" });
-});
+$(document).ready(
+	/*function(){
+  		alert("Hola");
+ 		$('.headerIndex').css({ 'width': $(window).width() + "px" });
+	}*/
+
+);
+
+
+function FindMe()
+{
+	var output=document.getElementById('map');//mostarmos si es compatible la geolocalización
+	var outputmapa=document.getElementById('mapa');// div para conetenr el mapa
+	if(navigator.geolocation)
+	{
+		output.innerHTML="<br> Soporta geolocalización";
+	}else
+	{
+			output.innerHTML="<br> No soporta geolocalización";
+	}
+	navigator.geolocation.getCurrentPosition(localization,error);
+	function localization(posicion)
+	{
+		var latitude= posicion.coords.latitude;
+		var longitude= posicion.coords.longitude;
+
+		//output.innerHTML="<p> Latitud: "+latitude+"<br> Logitud: "+longitude+"</p>";
+		var gLatLong= new google.maps.LatLng(latitude,longitude);
+		var objConfig=
+		{
+				zoom: 16,
+				center: gLatLong
+		}
+		var gMap= new google.maps.Map(outputmapa, objConfig);
+		var geocoder = new google.maps.Geocoder();//objeto para la geolocalización inversa
+		var infowindow = new google.maps.InfoWindow();//ventana de información sobre la dirección decodificada
+		var objConfigMarker=
+		{
+			position: gLatLong,
+			map: gMap
+		}
+		var gMarker= new google.maps.Marker(objConfigMarker);
+		geocodeLatLng(geocoder,gMap,infowindow,gLatLong,gMarker);
+		  
+	}
+	function error()
+	{
+		output.innerHTML="<br> No se pudo cargar el mapa. Recargue la página";
+	}
+	function geocodeLatLng(geocoder, gMap, infowindow, gLatLong, gMarker) 
+	{
+		geocoder.geocode({'location': gLatLong}, function(results, status) 
+		{
+			if (status =='OK') 
+			{
+				if (results[0])
+				{
+			 		infowindow.setContent(results[0].formatted_address);
+			 		infowindow.open(gMap, gMarker);
+			 	} else 
+			 	{
+			 		window.alert('No results found');
+			 	}
+			} else 
+			{
+				window.alert('Geocoder failed due to: ' + status);
+			}
+		})
+	}
+}
+
