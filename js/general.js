749e22ec8b67cbbdc14f404417f5475777b2ef3c
FerGlez230 <ferglez230@gmail.com>
2018-01-23 22:39:42 -0600

    Validacion mejorada

--- js/general.js
+++ js/general.js
@@ -5,84 +5,53 @@
 });*/
 function ValidarRegistroEstablecimiento()
 {
-	var NombreEstablecimiento=document.RegistroEstablecimientoFormulario.NombreEstablecimiento.value;
-	var Descripcion=document.RegistroEstablecimientoFormulario.Descripcion.value;
-	var Usuario=document.RegistroEstablecimientoFormulario.Usuario.value;
-	var ApellidoMaterno=document.RegistroEstablecimientoFormulario.ApellidoMaterno.value;
-	var ApellidoPaterno=document.RegistroEstablecimientoFormulario.ApellidoPaterno.value;
-	var Username=document.RegistroEstablecimientoFormulario.Username.value;
-	var CorreoElectronico=document.RegistroEstablecimientoFormulario.CorreoElectronico.value;
-	var Contrasena=document.RegistroEstablecimientoFormulario.Contrasena.value;
-	var ConfirmarContrasena=document.RegistroEstablecimientoFormulario.ConfirmarContrasena.value;
-	ValidarNoVacios(NombreEstablecimiento, "nombre de establecimiento");
-	ValidarNoVacios(Descripcion, "descripción");
-	ValidarTextComun(Usuario, "usuario");
-	ValidarTextComun(ApellidoPaterno, "apellido paterno");
-	if(ApellidoMaterno!=""){ValidarTextComun(ApellidoMaterno, "apellido materno");}
-	ValidarNoVacios(Username, "usuario");
-	ValidarCorreo(CorreoElectronico, "correo electrónico");
-	ValidarContrasena(Contrasena, "contraseña");
-	ValidarContrasena(ConfirmarContrasena, "confirmar contraseña");
-	ContrasenasIguales(Contrasena, ConfirmarContrasena);
-}
-function ValidarNoVacios(Campo,NombreCampo)
-{
-	if(Campo=="")
-	{
-		window.alert("Rellene el campo "+NombreCampo);
-		return false;
-	}
+	var contador=true;
+	var NombreEstablecimiento=document.RegistroEstablecimiento.NombreEstablecimiento.value;
+	var Descripcion=document.RegistroEstablecimiento.Descripcion.value;
+	var Usuario=document.RegistroEstablecimiento.Usuario.value;
+	var ApellidoMaterno=document.RegistroEstablecimiento.ApellidoMaterno.value;
+	var ApellidoPaterno=document.RegistroEstablecimiento.ApellidoPaterno.value;
+	var Username=document.RegistroEstablecimiento.Username.value;
+	var CorreoElectronico=document.RegistroEstablecimiento.CorreoElectronico.value;
+	var Contrasena=document.RegistroEstablecimiento.Contrasena.value;
+	var ConfirmarContrasena=document.RegistroEstablecimiento.ConfirmarContrasena.value;
+	contador=ValidarTextComun(Usuario, "usuario");
+	contador=ValidarTextComun(ApellidoPaterno, "apellido paterno");
+	if(ApellidoMaterno!=""){contador=ValidarTextComun(ApellidoMaterno, "apellido materno");}
+	contador=ValidarCorreo(CorreoElectronico);
+	contador=ValidarContrasena(Contrasena);
+	contador=ContrasenasIguales(Contrasena, ConfirmarContrasena);
+	return contador;
 }
 function ValidarTextComun(Campo, NombreCampo)
 {
 	var Letras = /^[a-zA-Z]*$/;
-	if(Campo=="")
+	if(!Campo.search(Letras))
 	{
-		window.alert("Rellene el campo "+NombreCampo);
-		return false;
 	}else
 	{
-		if(!Campo.search(Letras))
-		{
-		}else
-		{
-			window.alert("El campo "+NombreCampo+" sólo permite caracteres de la A-Z");
-			return false;
-		}	
+		window.alert("El campo "+NombreCampo+" sólo permite caracteres de la A-Z");
+		return false;
 	}
 }
-function ValidarCorreo(Campo, NombreCampo)
+function ValidarCorreo(Campo)
 {
 	var PatronCorreo=/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-	if(Campo=="")
+	if(Campo.search(PatronCorreo))
 	{
-		window.alert("Rellene el campo "+NombreCampo);
+		window.alert("Su correo probablemente tenga un error");
 		return false;
-	}else
-	{
-		if(Campo.search(PatronCorreo))
-		{
-			window.alert("Nope. Equivocado pacaa");
-			return false;
-		}
 	}
 }
-function ValidarContrasena(Campo,NombreCampo)
+function ValidarContrasena(Campo)
 {
 	var PatronContrasena=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d]|[^ ]){8,}/;//Busca por lo menos una minuscula, mayúscula, dígito y por lo menos 8 caracteres
-	if(Campo=="")
+	if(!Campo.search(PatronContrasena))
 	{
-		window.alert("Rellene el campo "+NombreCampo);
-		return false;
 	}else
-	{	
-		if(!Campo.search(PatronContrasena))
-		{
-		}else
-		{
-			alert("Su contraseña no coincide con alguno de los requisitos: \nUna mayúscula \nUna minúscula\nUna letra\nPor lo menos 8 caracteres\nSin espacios en blanco");
-			return false;
-		}
+	{
+		alert("Su contraseña no coincide con alguno de los requisitos: \nUna mayúscula \nUna minúscula\nUna letra\nPor lo menos 8 caracteres\nSin espacios en blanco");
+		return false;
 	}
 }
 function ContrasenasIguales(Contrasena, Contrasena2)
@@ -92,6 +61,32 @@ function ContrasenasIguales(Contrasena, Contrasena2)
 		window.alert("Las contraseñas no coinciden");
 		return false;
 	}
+}
+function NuevoHorario()
+{
+	var DiasSeleccionados= new Array();
+	var Registros=new Array();
+	var HoraInicio=document.RegistroEstablecimiento.HoraInicio.value;
+	var HoraTermino=document.RegistroEstablecimiento.HoraTermino.value;
+	var DiasCheck=document.getElementsByName('diasSemana');
+	if(HoraInicio>=HoraTermino)
+	{
+		window.alert("La hora de inicio no puede ser mayor a la hora de termino");
+		return false;
+	}
+	for(var i=0; i<7; i++)
+	{
+		if(DiasCheck[i].checked)
+		{
+			DiasSeleccionados[i]=i+1;
+			window.alert(DiasSeleccionados[i]);
+		}
+	}
+	/*for (var j = 0; j < DiasSeleccionados.length; j++) 
+	{
+		Registros[j]=(HoraInicio+HoraTermino+DiasSeleccionados[j]);
+	}*/
+
 }
 function FindMe()
 {
