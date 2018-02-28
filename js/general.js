99acf6c5784c87d858cb00893ed36beb6e815b9c
FerGlez230 <ferglez230@gmail.com>
2018-02-27 19:56:25 -0600

    Ultimos cambios

--- js/general.js
+++ js/general.js
@@ -243,6 +243,49 @@ function CargarMapaInicial()
 		output.innerHTML="<br> No se pudo cargar el mapa inicial. Recargue la página";
 	}
 }
+function FindMeByAddress()
+{
+	var outputmapa=document.getElementById('mapa');// div para conetenr el mapa
+	var geocoder = new google.maps.Geocoder();//objeto para la geolocalización inversa
+	var address = document.getElementById('address').value;
+	/*var options = {
+		types: ['(cities)'],
+		componentRestrictions: {country: 'mx'}
+		};
+	autocomplete = new google.maps.places.Autocomplete(address, options);
+	autocomplete_textarea = new google.maps.places.Autocomplete(address,
+		{ types: ['geocode'] });
+		google.maps.event.addListener(autocomplete_textarea, 'place_changed', function() {
+		fillInAddress_textarea();});
+	function fillInAddress_textarea(){
+		var place = autocomplete_textarea.getPlace();
+		console.log( place.formatted_address );
+		console.log( JSON.stringify(place) );
+		$('#address').val( place.formatted_address );
+	}*/
+	var latitude= 20.703004;
+	var longitude= -103.390141;
+	var gLatLong= new google.maps.LatLng(latitude,longitude);
+	var objConfig=
+	{
+		zoom: 16,
+		center: gLatLong
+	}
+	var gMap= new google.maps.Map(outputmapa, objConfig);
+	var infowindow = new google.maps.InfoWindow();//ventana de información sobre la dirección decodificada
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == 'OK') {
+			gMap.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+				map: gMap,
+				position: results[0].geometry.location});
+			infowindow.setContent(address);
+			infowindow.open(gMap, marker);
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});
+}
 function FindMe()
 {
 	var output=document.getElementById('map');//mostarmos si es compatible la geolocalización
@@ -255,6 +298,7 @@ function FindMe()
 		//output.innerHTML="<br> No soporta geolocalización";
 	}
 	navigator.geolocation.getCurrentPosition(localization,error);
+
 	function localization(posicion)
 	{
 		var latitude= posicion.coords.latitude;
@@ -275,8 +319,10 @@ function FindMe()
 			position: gLatLong,
 			map: gMap
 		}
+		
 		var gMarker= new google.maps.Marker(objConfigMarker);
 		geocodeLatLng(geocoder,gMap,infowindow,gLatLong,gMarker); 
+		 
 	}
 	function error()
 	{
